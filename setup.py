1a1f0dafab744e38a3351aa509f6bd9c8f5f9882
Johannes Schöneberger <hannes1033@gmail.com>
2024-06-02 23:55:42 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mchost24',
+      version='0.0.1',
+      description='A Python Module that allows interacting with the MC-Host24 REST API',
+      url='https://github.com/JoeJoeTV/mchost24-api-python',
+      author='JoeJoeTV',
+      author_email='joejoetv@joejoetv.de',
+      license='GPL-3.0',
+      packages=['mchost24'],
+      zip_safe=False)
